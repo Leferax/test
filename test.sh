998e81a2c202ccdb5304b1abc50fb60338b2747a
Leferax <lea.ferrandez@proton.me>
2025-04-07 10:27:35 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -187,11 +187,11 @@ usermod -aG sudo safyradmin
 echo "SAFYRADMIN_PASSWORD=${SAFYRADMIN_PASSWORD}" >> /root/.safyra_credentials
 
 cp -r /root/.ssh /root/.ssh.bak
-#mkdir -p /home/safyradmin/.ssh /root/.ssh
-#chmod 700 /home/safyradmin/.ssh /root/.ssh
-#cp /root/.ssh/authorized_keys /home/safyradmin/.ssh/authorized_keys 
-#chmod 600 /home/safyradmin/.ssh/authorized_keys /root/.ssh/authorized_keys
-#chown -R safyradmin:safyradmin /home/safyradmin/.ssh
+mkdir -p /home/safyradmin/.ssh
+chmod 700 /home/safyradmin/.ssh
+cat /root/.ssh/authorized_keys >> /home/safyradmin/.ssh/authorized_keys 
+chmod 600 /home/safyradmin/.ssh/authorized_keys
+chown -R safyradmin:safyradmin /home/safyradmin/.ssh
 
 pveum user add terraform@pve -comment "Terraform Automation"
 pveum role add TerraformRole -privs "VM.Allocate VM.Audit Datastore.AllocateSpace Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify VM.Clone VM.Config.CDROM VM.Config.CPU VM.Config.Cloudinit VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt SDN.Use"
@@ -205,7 +205,7 @@ chmod 600 /root/.safyra_credentials
 lynis audit system > /root/lynis_report.txt
 chmod 600 /root/lynis_report.txt
 
-#systemctl restart pveproxy
+systemctl restart pveproxy
 systemctl restart networking
 
 echo "[DONE] SAFYRA INSTALL - $(date)"
